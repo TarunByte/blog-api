318b03cfb8c5572d897617a4ae8a25baac8869b2
TarunBits <tarunbhattmortal@gmail.com>
2025-11-26 21:30:45 +0530

    Get current user

---
+++ src/routes/v1/user.ts
@@ -0,0 +1,36 @@
+/**
+ * @copyright 2025 codewithsadee
+ * @license Apache-2.0
+ */
+
+/**
+ * Node modules
+ */
+import { Router } from "express";
+
+/**
+ * Middlewares
+ */
+import authenticate from "@/middleware/authenticate";
+import authorize from "@/middleware/authorize";
+
+/**
+ * Controllers
+ */
+import getCurrentUser from "@/controller/v1/user/get_current_user";
+
+/**
+ * Models
+ */
+import User from "@/models/user";
+
+const router = Router();
+
+router.get(
+  "/current",
+  authenticate,
+  authorize(["admin", "user"]),
+  getCurrentUser
+);
+
+export default router;
