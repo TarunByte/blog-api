48c44e0d4ba45bb95c247d493f9ece253c2854f0
TarunBits <tarunbhattmortal@gmail.com>
2026-01-08 23:20:01 +0530

    FrontEnd Initial setup and FronEnd Login

---
+++ frontend/src/main.tsx
@@ -0,0 +1,33 @@
+/**
+ * @copyright 2025 codewithsadee
+ * @license Apache-2.0
+ */
+
+/**
+ * Node modules
+ */
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router";
+/**
+ * Css
+ */
+import "./index.css";
+
+/**
+ * Router
+ */
+import router from "@/routes/";
+
+/**
+ * Components
+ */
+import { ThemeProvider } from "@/components/ThemeProvider";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </StrictMode>
+);
---
+++ frontend/src/routes/index.ts
@@ -0,0 +1,81 @@
+/**
+ * @copyright 2025 codewithsadee
+ * @license Apache-2.0
+ */
+
+/**
+ * Node modules
+ */
+import { createBrowserRouter } from "react-router";
+
+/**
+ * Loaders
+ */
+
+/**
+ * Pages
+ */
+import { Login } from "@/pages/auth/Login";
+
+/**
+ * Actions
+ */
+
+/**
+ * Error boundaries
+ */
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    Component: Login,
+  },
+  {
+    path: "/signup",
+  },
+  {
+    path: "/refresh-token",
+  },
+  {
+    path: "/",
+    children: [
+      {
+        index: true,
+      },
+      {
+        path: "blogs",
+      },
+      {
+        path: "blogs/:slug",
+      },
+    ],
+  },
+  {
+    path: "/admin",
+    children: [
+      {
+        path: "dashboard",
+      },
+      {
+        path: "blogs",
+      },
+      {
+        path: "blogs/create",
+      },
+      {
+        path: "blogs/:slug/edit",
+      },
+      {
+        path: "comments",
+      },
+      {
+        path: "users",
+      },
+    ],
+  },
+  {
+    path: "/settings",
+  },
+]);
+
+export default router;
